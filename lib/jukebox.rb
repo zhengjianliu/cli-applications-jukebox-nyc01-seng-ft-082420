e81f3853c3efcfa7268594e63c1b5d16123f9ecd
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-22 14:28:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   songs.each_with_index { |song, index|
-    if input == index.to_s || input == song
+    if input == index || input == song
       output = "Playing #{song}"
       break
     else
